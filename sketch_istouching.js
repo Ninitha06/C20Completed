a5a1ff9cadf771ce0ad5b57668fd58c14b180f25
Ninitha <67736136+Ninitha06@users.noreply.github.com>
2020-08-23 17:16:12 +0530

    Update sketch_istouching.js

--- sketch_istouching.js
+++ sketch_istouching.js
@@ -1,10 +1,10 @@
 var fixedRect, movingRect;
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect = createSprite(600, 400, 50, 80);
   fixedRect.shapeColor = "green";
   fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
+  movingRect = createSprite(800,400,80,30);
   movingRect.shapeColor = "green";
   movingRect.debug = true;
 }
